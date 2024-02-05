99a1371c9906180d1f545c61c9f1adade4d1e1e3
mlucero-87 <matiaslucero87@hotmail.com>
2024-02-05 12:32:37 -0300

    Add DbContext

---
+++ Application/Abstractions/IChallengeDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Abstractions
+{
+    public interface IChallengeDbContext
+    {
+        DbSet<Contact> Contacts { get; set; }
+        DbSet<Address> Address { get; set; }
+    }
+}
---
+++ Infrastructure/Persistence/ChallengeDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public partial class ChallengeDbContext : DbContext, IChallengeDbContext
+    {
+
+        public ChallengeDbContext(DbContextOptions<ChallengeDbContext> options)
+        : base(options)
+        {
+        }
+        public virtual DbSet<Contact> Contacts { get; set; }
+        public virtual DbSet<Address> Address { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite($"Data Source=contacts.db");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
