c3b635c59ca244867572346bf36a465760811d41
mlucero-87 <matiaslucero87@hotmail.com>
2024-02-05 11:22:30 -0300

    Agregar archivos de proyecto.

---
+++ Domain/Entities/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Profile { get; set; }
+        public string Email { get; set; }
+        public string Image { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string PersonalPhoneNumber { get; set; }
+        public string WorkPhoneNumber { get; set; }
+        public virtual Address Address { get; set; }
+
+    }
+}
